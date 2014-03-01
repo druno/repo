055a24818954229b4f838601eaf27b663fa1b140
druno <druno@MacBook-Air-Andrej-2.local>
2014-03-01 15:01:55 +0400

    rtc

--- Hydro/Hydro.ino
+++ Hydro/Hydro.ino
@@ -1,9 +1,19 @@
+#include <Wire.h>
+#include <RTClib.h>
+
+RTC_DS1307 RTC;
+
 void setup()
 {
+  
+ 
+  Wire.begin();
+  RTC.begin();
+  
   pinMode(2, OUTPUT); //led1
   digitalWrite(2, LOW); //сброс на 0  
-  pinMode(14, OUTPUT);  //led2
-  digitalWrite(14, LOW); //сброс на 0
+ // pinMode(14, OUTPUT);  //led2
+ // digitalWrite(14, LOW); //сброс на 0
 
   pinMode(19, OUTPUT);  //смеситель
   digitalWrite(19, LOW); //сброс на 0 
@@ -22,7 +32,7 @@ void loop()
 {
    if (voda()>0)       //уровень воды нормальный
    {
-    digitalWrite(14, HIGH); //зажигаем зеленый
+  //  digitalWrite(14, HIGH); //зажигаем зеленый
     digitalWrite(2, LOW); //гасим красный
 
     unsigned long currentMillis = millis();     //время с момента последнего запуска или резета
@@ -37,9 +47,26 @@ void loop()
     }
     else {     //Уровень воды ниже нормы 
        digitalWrite(2, HIGH); //зажигаем красный
-       digitalWrite(14, LOW); //гасим зеленый
+  //     digitalWrite(14, LOW); //гасим зеленый
     } 
+  
+    DateTime now = RTC.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();  
+    
+    
   delay(100);     //задержка на каждый цикл  
+  
 }                 //end main loop
 
 
