b6b22a7e88e295bce098548686c4c5726c204fe3
druno <druno@MacBook-Air-Andrej-2.local>
2014-03-20 21:26:07 +0400

    новая

--- Hydro/Hydro.ino
+++ Hydro/Hydro.ino
@@ -1,87 +1,128 @@
 #include <Wire.h>
 #include <RTClib.h>
+//#include <LiquidCrystal.h>
 
 RTC_DS1307 RTC;
+//LiquidCrystal lcd(P1_0, P1_1, P1_2, P1_3, P1_4, P1_5);
 
 void setup()
 {
-  
- 
   Wire.begin();
-  RTC.begin();
-  
+  RTC.begin(); 
   pinMode(2, OUTPUT); //led1
   digitalWrite(2, LOW); //сброс на 0  
- // pinMode(14, OUTPUT);  //led2
- // digitalWrite(14, LOW); //сброс на 0
+
+  pinMode(5, INPUT_PULLUP);  //кнопень
 
   pinMode(19, OUTPUT);  //смеситель
   digitalWrite(19, LOW); //сброс на 0 
 
   Serial.begin(9600);
+  
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");  
 
 }
 
 long previousMillis = 0;  
-long interval = 10000; //интервал между поливами
+long interval = 360000; //интервал между поливами
 long smesitel = 2000; //время работы смесителя
 
 
 /////////////////////////////////////////////////
 void loop()
 {
-   if (voda()>0)       //уровень воды нормальный
-   {
-  //  digitalWrite(14, HIGH); //зажигаем зеленый
-    digitalWrite(2, LOW); //гасим красный
+  
+//  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+//  lcd.print(millis()/1000);
 
-    unsigned long currentMillis = millis();     //время с момента последнего запуска или резета
-    long time=currentMillis - previousMillis;
-    long time1=(interval-time)/1000; //Оставшееся время до запуска в sec
-     
-    Serial.println(time1); 
-    if(time > interval) {
-        previousMillis = currentMillis;   
-        poliv ();
-        }
-    }
-    else {     //Уровень воды ниже нормы 
-       digitalWrite(2, HIGH); //зажигаем красный
-  //     digitalWrite(14, LOW); //гасим зеленый
+  
+   if (digitalRead(5) == LOW) {    //полив по нажатию кнопки вне очереди
+      poliv ();  
     } 
   
+   if (voda()>0)       //уровень воды нормальный
+   {
+      digitalWrite(2, LOW); //гасим красный
+  
+      unsigned long currentMillis = millis();     //время с момента последнего запуска или резета
+      long time=currentMillis - previousMillis;
+      long time1=(interval-time)/1000; //Оставшееся время до запуска в sec
+       
+      Serial.println(time1); 
+      if(time > interval) {
+          previousMillis = currentMillis;   
+          poliv ();
+          }
+    }
+    else {    
+       digitalWrite(2, HIGH); //зажигаем красный
+    }
+   
     DateTime now = RTC.now();
     Serial.print(now.year(), DEC);
     Serial.print('/');
+   if (now.month() <10) Serial.print('0');    
     Serial.print(now.month(), DEC);
     Serial.print('/');
+    if (now.day() <10) Serial.print('0');    
     Serial.print(now.day(), DEC);
     Serial.print(' ');
+    if (now.hour() <10) Serial.print('0');
     Serial.print(now.hour(), DEC);
     Serial.print(':');
+    if (now.minute() <10) Serial.print('0');
     Serial.print(now.minute(), DEC);
     Serial.print(':');
+    if (now.second() <10) Serial.print('0');
     Serial.print(now.second(), DEC);
     Serial.println();  
+
+   if ((now.hour() >= 8) and (now.hour() <=10) ) {
+          Serial.print(now.hour());
+       Serial.println(" svet on "); 
+      svet(); 
+    } 
+   else {    
+     Serial.print(now.hour());
+       Serial.println(" svet off ");
+    }
     
     
-  delay(100);     //задержка на каждый цикл  
+  delay(1000);     //задержка на каждый цикл  
   
 }                 //end main loop
 
 
 
-//////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
 int voda () {
-//рандомно проверяем уровень воды, если 1 то норм
+//проверяем уровень воды, если 1 то норм
   return 1; //random (2);
 }
 
 void poliv() {
 //процедура полива
-    digitalWrite(19, HIGH);  
+    digitalWrite(19, HIGH); 
+    Serial.println("idet poliv"); 
     delay (smesitel);
     digitalWrite(19, LOW);  
-    Serial.println("poliv");
+    Serial.println("poliv okonchen");
+    delay (800);
     return;  
 }
+
+void svet() {
+//процедура полива
+    digitalWrite(2, HIGH); 
+    delay (1000);
+    digitalWrite(2, LOW);  
+    delay (1000);
+    return;  
+}
+
+
